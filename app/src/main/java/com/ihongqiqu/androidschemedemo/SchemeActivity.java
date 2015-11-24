a346509d0009165fc4256cf380551e47c98f7dde
Zhenguo <jingle1267@163.com>
2015-11-24 18:14:28 +0800

    [ADD] Add another callback.

--- app/src/main/java/com/ihongqiqu/androidschemedemo/SchemeActivity.java
+++ app/src/main/java/com/ihongqiqu/androidschemedemo/SchemeActivity.java
@@ -1,13 +1,34 @@
 package com.ihongqiqu.androidschemedemo;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SchemeActivity extends AppCompatActivity {
+
+    TextView tv_data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scheme);
+        tv_data = (TextView) findViewById(R.id.tv_data);
+
+        Uri uri = getIntent().getData();
+        StringBuilder sb = new StringBuilder();
+        sb.append("scheme:");
+        sb.append(uri.getScheme());
+        sb.append("\n");
+        sb.append("host:");
+        sb.append(uri.getHost());
+        sb.append("\n");
+        sb.append("port:");
+        sb.append(uri.getPort());
+        sb.append("\n");
+        sb.append("path:");
+        sb.append(uri.getPath());
+
+        tv_data.setText(sb.toString());
     }
 }
